22d5d947b337c8c5eb759a7b54053c634d35368a
Marius <53195616+mariushub@users.noreply.github.com>
2026-02-10 14:09:34 +0100

    cleaning removing useless function and variables

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -5,6 +5,7 @@ const urlParams = new URLSearchParams(window.location.search);
 async function fetchData(url) {
   try {
     const response = await fetch(url);
+
     if (!response.ok) {
       throw new Error(`Response status: ${response.status}`);
     }
@@ -13,8 +14,8 @@ async function fetchData(url) {
 
     return result;
   } catch (error) {
-    console.error(error.message);
-    return error;
+    console.error("error fetching data : " + error.message);
+    return false;
   }
 }
 
@@ -24,25 +25,30 @@ async function loadWorks() {
   const works = await fetchData("http://localhost:5678/api/works");
 
   // create all works from api and add it to the dom
-  works.forEach((work) => {
-    let figure = document.createElement("figure");
-    let image = document.createElement("img");
-    let caption = document.createElement("figcaption");
+  if (works) {
+    works.forEach((work) => {
+      let figure = document.createElement("figure");
+      let image = document.createElement("img");
+      let caption = document.createElement("figcaption");
 
-    figure.dataset.id = work.id;
-    figure.dataset.category = work.categoryId;
-    figure.id = "work-" + work.id;
+      figure.dataset.id = work.id;
+      figure.dataset.category = work.categoryId;
+      figure.id = "work-" + work.id;
 
-    image.src = work.imageUrl;
-    image.alt = work.title;
+      image.src = work.imageUrl;
+      image.alt = work.title;
 
-    caption.innerText = work.title;
+      caption.innerText = work.title;
 
-    figure.appendChild(image);
-    figure.appendChild(caption);
+      figure.appendChild(image);
+      figure.appendChild(caption);
 
-    worksContainer.appendChild(figure);
-  });
+      worksContainer.appendChild(figure);
+    });
+  } else {
+    console.warn("works is not valid");
+    return;
+  }
 }
 
 // get filters from api and add them to DOM
@@ -55,15 +61,117 @@ async function loadFilters() {
   tousBtn.addEventListener("click", handleFilterChange);
 
   // create all the filter btn from api and add it to the dom
-  filters.forEach((filter) => {
-    let btn = document.createElement("button");
-    btn.innerText = filter.name;
-    btn.id = filter.id;
-    btn.classList = "filter";
-    btn.addEventListener("click", handleFilterChange);
+  if (filters) {
+    filters.forEach((filter) => {
+      let btn = document.createElement("button");
+      btn.innerText = filter.name;
+      btn.id = filter.id;
+      btn.classList = "filter";
+      btn.addEventListener("click", handleFilterChange);
 
-    filtersContainer.appendChild(btn);
-  });
+      filtersContainer.appendChild(btn);
+    });
+  } else {
+    console.warn("filters is not valid");
+    return;
+  }
+}
+
+async function loadCreateModale() {
+  const categories = await fetchData("http://localhost:5678/api/categories");
+  const categoriesSelect = document.querySelector("#categoryForm");
+  const form = document.querySelector("#galleryAddForm");
+
+  // file droping logic
+  function initFile() {
+    let fileBtn = document.querySelector("#fileBtn");
+    let fileImage = document.querySelector(".image");
+    let fileInput = document.querySelector("#fileInput");
+
+    // file input is mapped to html
+    fileBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+      fileInput.click();
+    });
+
+    // when the user select an image we show it
+    fileInput.addEventListener("change", (e) => {
+      let file = fileInput.files[0];
+      let url = window.URL.createObjectURL(file);
+      let img = document.createElement("img");
+
+      img.src = url;
+
+      fileImage.childNodes[1].remove();
+      fileImage.childNodes[2].remove();
+      fileImage.childNodes[3].remove();
+
+      fileImage.appendChild(img);
+    });
+  }
+
+  if (categories) {
+    // load all categories
+    categories.forEach((categorie, index) => {
+      let newCategorie = document.createElement("option");
+      newCategorie.value = categorie.id;
+      newCategorie.innerText = categorie.name;
+
+      categoriesSelect.appendChild(newCategorie);
+    });
+
+    initFile();
+
+    // when form is submited we post it to the api
+    form.addEventListener("submit", async (e) => {
+      e.preventDefault();
+      const formData = new FormData(form);
+      const session = window.localStorage.getItem("session");
+
+      // update object in backend
+      const response = await fetch(`http://localhost:5678/api/works/`, {
+        method: "POST",
+        body: formData,
+        headers: {
+          Authorization: `Bearer ${session}`,
+        },
+      });
+
+      // check if upload work
+      if (!response.ok) {
+        console.error("error posting work : " + response.status);
+        return;
+      }
+
+      // close the modal
+      document.getElementById("edit").classList = "hide";
+
+      // create the new work
+      const data = await response.json();
+      console.log(data);
+      const worksContainer = document.querySelector(".gallery");
+      let figure = document.createElement("figure");
+      let image = document.createElement("img");
+      let caption = document.createElement("figcaption");
+
+      figure.dataset.id = data.id;
+      figure.dataset.category = data.categoryId;
+      figure.id = "work-" + data.id;
+
+      image.src = data.imageUrl;
+      image.alt = data.title;
+
+      caption.innerText = data.title;
+
+      figure.appendChild(image);
+      figure.appendChild(caption);
+
+      worksContainer.appendChild(figure);
+    });
+  } else {
+    console.warn("categories in create modal is not valid");
+    return;
+  }
 }
 
 // this function take a event as params or id direcly
@@ -100,216 +208,104 @@ function handleFilterChange(e) {
 }
 
 // reset localstorage and reload page
-function handleLogout(e) {
+function handleLogout() {
   localStorage.removeItem("session");
   location.reload();
 }
 
-function handleCloseModale(e) {
-  document.getElementById("edit").classList = "hide";
-  console.log("aa");
-}
-
-async function handleShowModale(e) {
-  // show the edit section
-  document.getElementById("edit").classList = "";
-  document.getElementById("galleryEdit").classList = "galleryEdit";
-  document.getElementById("galleryAdd").classList = "galleryAdd hide";
+async function handleShowModale() {
   // get the work from api
   const list = document.querySelector(".galleryEdit__content");
   const works = await fetchData("http://localhost:5678/api/works");
 
-  // reset any old content
-  list.innerHTML = "";
+  if (works) {
+    // show the edit section
+    document.getElementById("edit").classList = "";
+    document.getElementById("galleryEdit").classList = "galleryEdit";
+    document.getElementById("galleryAdd").classList = "galleryAdd hide";
 
-  // create all works from api and add it to the dom
-  works.forEach((work) => {
-    let card = document.createElement("div");
-    let image = document.createElement("img");
-    let btn = document.createElement("button");
-    let ico = document.createElement("i");
-    card.classList = "galleryEdit__card";
-    card.id = "modal-" + work.id;
+    // reset any old content
+    list.innerHTML = "";
 
-    ico.classList = "fa-solid fa-trash-can";
-    ico.id = work.id;
+    // create all works from api and add it to the dom
+    works.forEach((work) => {
+      let card = document.createElement("div");
+      let image = document.createElement("img");
+      let btn = document.createElement("button");
+      let ico = document.createElement("i");
+      card.classList = "galleryEdit__card";
+      card.id = "modal-" + work.id;
 
-    image.src = work.imageUrl;
+      ico.classList = "fa-solid fa-trash-can";
+      ico.id = work.id;
 
-    btn.id = work.id;
-    btn.addEventListener("click", async (e) => {
-      const session = window.localStorage.getItem("session");
-      const id = e.target.id;
+      image.src = work.imageUrl;
 
-      const response = await fetch(`http://localhost:5678/api/works/${id}`, {
-        method: "DELETE",
-        headers: {
-          accept: "application/json",
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${session}`,
-        },
+      btn.id = work.id;
+      btn.addEventListener("click", async (e) => {
+        const session = window.localStorage.getItem("session");
+        const id = e.target.id;
+
+        const response = await fetch(`http://localhost:5678/api/works/${id}`, {
+          method: "DELETE",
+          headers: {
+            accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${session}`,
+          },
+        });
+
+        if (response.ok) {
+          console.log(response);
+
+          let deletedImageModal = document.querySelector("#modal-" + work.id);
+          deletedImageModal.remove();
+
+          let deletedWork = document.querySelector("#work-" + work.id);
+          deletedWork.remove();
+        } else {
+          console.log(response);
+        }
       });
 
-      if (response.ok) {
-        console.log(response);
+      btn.appendChild(ico);
+      card.appendChild(image);
+      card.appendChild(btn);
 
-        let deletedImageModal = document.querySelector("#modal-" + work.id);
-        deletedImageModal.remove();
-
-        let deletedWork = document.querySelector("#work-" + work.id);
-        deletedWork.remove();
-      } else {
-        console.log(response);
-      }
+      list.appendChild(card);
     });
 
-    btn.appendChild(ico);
-    card.appendChild(image);
-    card.appendChild(btn);
-
-    list.appendChild(card);
-  });
-
-  galleryAddBtn.addEventListener("click", () => {
-    document.getElementById("edit").classList = "";
-    document.getElementById("galleryEdit").classList = "galleryEdit hide";
-    document.getElementById("galleryAdd").classList = "galleryAdd";
-  });
-
-  document.querySelector(".galleryAdd__back").addEventListener("click", () => {
-    document.getElementById("edit").classList = "";
-    document.getElementById("galleryEdit").classList = "galleryEdit ";
-    document.getElementById("galleryAdd").classList = "galleryAdd hide";
-  });
-}
-
-// load nav direcly in js to prevent flashing on load
-function renderNav(links) {
-  let nav = document.getElementById("nav");
-
-  links.forEach((link) => {
-    let parent = document.createElement("li");
-    let child = document.createElement(link.type);
-
-    switch (link.type) {
-      case "a": {
-        child.innerText = link.text;
-        child.href = link.href;
-      }
-
-      case "img": {
-        child.src = link.src;
-      }
-
-      case "button": {
-        child.innerText = link.text;
-
-        if (link.text === "logout") {
-          child.addEventListener("click", handleLogout);
-        }
-      }
-    }
-
-    parent.appendChild(child);
-    nav.appendChild(parent);
-  });
-}
-
-async function handleCreateModale(e) {
-  // file droping logic
-  let fileBtn = document.querySelector("#fileBtn");
-  let fileImage = document.querySelector(".image");
-  let fileInput = document.querySelector("#fileInput");
-  let form = document.querySelector("#galleryAddForm");
-
-  // load all categories
-  const categories = await fetchData("http://localhost:5678/api/categories");
-  let categoriesSelect = document.querySelector("#categoryForm");
-  categories.forEach((categorie, index) => {
-    let newCategorie = document.createElement("option");
-    newCategorie.value = categorie.id;
-    newCategorie.innerText = categorie.name;
-
-    categoriesSelect.appendChild(newCategorie);
-  });
-
-  // file input is mapped to html
-  fileBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    fileInput.click();
-  });
-
-  // when the user select an image we show it
-  fileInput.addEventListener("change", (e) => {
-    let file = fileInput.files[0];
-    let url = window.URL.createObjectURL(file);
-    let img = document.createElement("img");
-
-    img.src = url;
-    console.log(fileImage.childNodes);
-
-    fileImage.childNodes[1].remove();
-    fileImage.childNodes[2].remove();
-    fileImage.childNodes[3].remove();
-
-    fileImage.appendChild(img);
-  });
-
-  // when form is submited we post it to the api
-  form.addEventListener("submit", async (e) => {
-    e.preventDefault();
-    const formData = new FormData(form);
-    const session = window.localStorage.getItem("session");
-
-    // update object in back
-    const data = await fetch(`http://localhost:5678/api/works/`, {
-      method: "POST",
-      body: formData,
-      headers: {
-        Authorization: `Bearer ${session}`,
-      },
-    })
-    // check everything went well
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      return response.json();
+    // show the createModale
+    galleryAddBtn.addEventListener("click", () => {
+      document.getElementById("edit").classList = "";
+      document.getElementById("galleryEdit").classList = "galleryEdit hide";
+      document.getElementById("galleryAdd").classList = "galleryAdd";
     });
 
-    handleCloseModale();
-    console.log(data);
+    document
+      .querySelector(".galleryAdd__back")
+      .addEventListener("click", () => {
+        document.getElementById("edit").classList = "";
+        document.getElementById("galleryEdit").classList = "galleryEdit ";
+        document.getElementById("galleryAdd").classList = "galleryAdd hide";
+      });
+  } else {
+    console.warn("in showmodale works are not valid");
+  }
+}
 
-    // create the new work
-    const worksContainer = document.querySelector(".gallery");
-    let figure = document.createElement("figure");
-    let image = document.createElement("img");
-    let caption = document.createElement("figcaption");
-
-    figure.dataset.id = data.id;
-    figure.dataset.category = data.categoryId;
-    figure.id = "work-" + data.id;
-
-    image.src = data.imageUrl;
-    image.alt = data.title;
-
-    caption.innerText = data.title;
-
-    figure.appendChild(image);
-    figure.appendChild(caption);
-
-    worksContainer.appendChild(figure);
-  });
+// only call in one dom element maybe add listener
+function handleCloseModale() {
+  document.getElementById("edit").classList = "hide";
 }
 
 // here we load all function and do stuff on page load
 async function pageLoad() {
   const id = urlParams.get("filter");
 
-  // in the future we could add the id here so we don't flash all content on load
   await loadWorks();
   await loadFilters();
-  await handleCreateModale();
+  await loadCreateModale();
 
   // if we have an id from url params then we select it else we just use tous as default
   if (id) {
@@ -321,18 +317,13 @@ async function pageLoad() {
   // hide login if user is sign in, hide logout if user is not sign in
   if (window.localStorage.getItem("session")) {
     document.getElementById("login").classList = "hide";
+    document.getElementById("logout").classList = "";
     document.getElementById("editBtn").classList = "";
   } else {
+    document.getElementById("login").classList = "";
     document.getElementById("logout").classList = "hide";
     document.getElementById("editBtn").classList = "hide";
   }
-
-  // add some event
-  document.querySelector(".backdrop").addEventListener("click", () => {
-    document.getElementById("edit").classList = "hide";
-    document.getElementById("galleryEdit").classList = "galleryEdit hide";
-    document.getElementById("galleryAdd").classList = "galleryAdd hide";
-  });
 }
 
 pageLoad();
--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,55 +1,60 @@
 async function handleSubmit(e) {
-    e.preventDefault()
-    const formData = new FormData(document.querySelector("#login"));
+  e.preventDefault();
+  const formData = new FormData(document.querySelector("#login"));
 
-    try {
-        // post the form data to api
-        const response = await fetch("http://localhost:5678/api/users/login", {
-            method: "POST",
-            body: JSON.stringify({
-                email: formData.get("email"),
-                password: formData.get("password"),
-            }),
-            headers: {
-                'accept': 'application/json',
-                'Content-Type': 'application/json',
-            },
-        });
+  try {
+    // post the form data to api
+    const response = await fetch("http://localhost:5678/api/users/login", {
+      method: "POST",
+      body: JSON.stringify({
+        email: formData.get("email"),
+        password: formData.get("password"),
+      }),
+      headers: {
+        accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    });
 
-        // switch case between response code error 
-        if (!response.ok) {
-            if (response.status === 401) {
-                alert("mot de passe incorecte")
-                document.getElementsByClassName("error").classlist = "error"
-                document.getElementsByClassName("error").innerText = "qsfqsfqsf"
-                return
-            } else if (response.status === 404) {
-                alert("utilisateur inconu")
-                return
-            } else {
-                alert("erreur server")
-                throw new Error(`Response status: ${response.status}`);
-                return
-            }
-        }
-
-        // saving session and redirecting to home
-        const result = await response.json();
-        window.localStorage.setItem("session", result.token)
-        window.location.replace("http://127.0.0.1:5501/FrontEnd/index.html");
-
-        return
-    } catch (error) {
-        console.error(error.message);
-        return
+    // switch case between response code error
+    if (!response.ok) {
+      if (response.status === 401) {
+        alert("mot de passe incorecte");
+        document.getElementsByClassName("error").classlist = "error";
+        document.getElementsByClassName("error").innerText = "qsfqsfqsf";
+        return;
+      } else if (response.status === 404) {
+        alert("utilisateur inconu");
+        return;
+      } else {
+        alert("erreur server");
+        throw new Error(`Response status: ${response.status}`);
+        return;
+      }
     }
+
+    // saving session and redirecting to home
+    const result = await response.json();
+    window.localStorage.setItem("session", result.token);
+    window.location.replace("http://127.0.0.1:5501/FrontEnd/index.html");
+
+    return;
+  } catch (error) {
+    console.error(error.message);
+    return;
+  }
 }
 
 // here we load all function and do stuff on page load
-async function pageLoad() {
-    const form = document.querySelector("#login")
+function pageLoad() {
+  const form = document.querySelector("#login");
 
-    form.addEventListener('submit', handleSubmit)
+  // if user is already logged in we redirect him to home page
+  if (window.localStorage.getItem("session")) {
+    window.location.href = "http://127.0.0.1:5501/FrontEnd/index.html";
+  }
+
+  form.addEventListener("submit", handleSubmit);
 }
 
-pageLoad()
+pageLoad();
